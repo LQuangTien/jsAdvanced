f34b333da7a9061738e803f51773efc38caaaf69
LQuangTien <tienkg5554@gmail.com>
2020-05-01 20:18:15 +0700

    arrow function part 1

---
+++ arrowFuncion/index.js
@@ -0,0 +1,8 @@
+// PART 1
+var sum = (a,b) => a+b
+console.log('sum: ', sum(3,2))
+var square = x => x*x;
+console.log('square: ', square(2))
+var arr = [1, 3, 5]
+var newArr = arr.map((x) => x*2 )
+console.log('new arr: ', newArr)
