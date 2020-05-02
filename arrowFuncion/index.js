65dd44c45f3d0cda266f864222bfc0e8b94a2127
LQuangTien <tienkg5554@gmail.com>
2020-05-02 14:10:48 +0700

    arrow function part 2

--- arrowFuncion/index.js
+++ arrowFuncion/index.js
@@ -6,3 +6,15 @@ console.log('square: ', square(2))
 var arr = [1, 3, 5]
 var newArr = arr.map((x) => x*2 )
 console.log('new arr: ', newArr)
+
+// PART 2
+var a = {
+  name: 'AAA',
+  run: function(){
+    var run2= () => {
+      console.log(this.name)
+    }
+    run2()
+  }
+}
+a.run()
