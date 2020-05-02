8a10db0f71ed1842d93afac51009b1fa35db31fe
LQuangTien <tienkg5554@gmail.com>
2020-05-02 16:56:38 +0700

    template string

---
+++ templateString/index.js
@@ -0,0 +1,12 @@
+var greeting = function(name){
+  return 'Hi, ' + name + '!'
+}
+console.log(greeting("Tien"))   // use ' '
+
+var greeting_2 = function(name){
+  return `Hi, 
+    
+    
+    ${name}!`
+}
+console.log(greeting_2('Tien')) // use ` `
