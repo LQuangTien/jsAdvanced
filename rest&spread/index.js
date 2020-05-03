e4d635a9345b7e6f7c31ab9fb47ef6765a9dff31
LQuangTien <tienkg5554@gmail.com>
2020-05-03 19:16:22 +0700

    shallow clone and deep clone

--- rest&spread/index.js
+++ rest&spread/index.js
@@ -16,3 +16,27 @@ const a = [1, 2, 3]
 const b = [0, ...a, 4]
 console.log(b)
 console.log(sum(...b)) // sum(0, 1, 2, 3, 4)
+
+//shalow clone
+obj1 ={
+  a: 1,
+  b: 2,
+  c: 3,
+  d: {e: 10}
+}
+
+obj2={
+  ...obj1,
+  z: 20
+}
+
+//deep clone
+obj3={
+  ...obj1,
+  z: 20,
+  d: {...obj1.d}
+}
+console.log({obj1, obj2, obj3}) //shallow clone
+obj2.d.e=12
+obj3.d.e=190
+console.log({obj1, obj2, obj3}) //deep clone
